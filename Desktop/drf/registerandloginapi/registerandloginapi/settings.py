65bf451d2b1681bad3091967abae5d8e56a0e0d2
parassarna <101689693+abhinandan98822@users.noreply.github.com>
2022-08-19 13:19:53 +0530

    smtpchanges

--- Desktop/drf/registerandloginapi/registerandloginapi/settings.py
+++ Desktop/drf/registerandloginapi/registerandloginapi/settings.py
@@ -133,7 +133,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
 EMAIL_HOST_USER = 'loveaggarwal179@gmail.com'  
-EMAIL_HOST_PASSWORD = 'zxlsrsmpqlkgwqsy' 
+EMAIL_HOST_PASSWORD = 'qyxpakohlhemwtgh' 
 
 
 REST_FRAMEWORK = {
