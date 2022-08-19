44a595b3903de8015e3308e3ea087eb509eff3b9
parassarna <101689693+abhinandan98822@users.noreply.github.com>
2022-08-19 11:21:51 +0530

    fkjkfkg

--- Desktop/drf/registerandloginapi/registerandloginapi/settings.py
+++ Desktop/drf/registerandloginapi/registerandloginapi/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
    
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'registerandloginapi.urls'
 
 TEMPLATES = [
@@ -162,8 +164,6 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
