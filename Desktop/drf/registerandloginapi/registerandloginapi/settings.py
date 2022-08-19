ace1c1376609fbeecc8811db11b125ee716ede9c
parassarna <101689693+abhinandan98822@users.noreply.github.com>
2022-08-19 15:20:52 +0530

    jfhkgljd

--- Desktop/drf/registerandloginapi/registerandloginapi/settings.py
+++ Desktop/drf/registerandloginapi/registerandloginapi/settings.py
@@ -132,8 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
-EMAIL_HOST_USER = 'loveaggarwal179@gmail.com'  
-EMAIL_HOST_PASSWORD = 'qyxpakohlhemwtgh' 
+EMAIL_HOST_USER = 'loveaggarwal@snakescript.com'  
+EMAIL_HOST_PASSWORD = 'eknecpfxbhjytknt' 
 
 
 REST_FRAMEWORK = {
